46a5fc0b3deb449beae6347eb3b14dede0347c35
Niki Balajadia <niki@Nikis-MacBook-Pro.local>
2022-09-13 12:20:21 -1000

    CDM done

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todo: [],
+    text: ''
+  }
+  handleChange = e => {
+    this.setState({
+      ...this.state,
+      text: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventdefault()
+
+  }
+
+  addTodos = () => {
+
+  }
+
+  componentDidmount() {
+    axios.get(URL)
+      .then(res => {
+        this.setState({
+          ...this.state,
+          todo: res.data.data
+        })
+      })
+      .catch(err => {
+        console.log({ err })
+      })
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <TodoList />
+        <Form />
+      </div>
+    )
   }
 }
